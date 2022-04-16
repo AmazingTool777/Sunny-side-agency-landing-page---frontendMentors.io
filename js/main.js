adc4ec8ee27d0b8d990df01242fe8cddc8de7e7f
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-04-16 22:55:11 +0300

    Navbar at mobile viewport

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+// Navbar toggler
+const navbarToggler = document.getElementById("navbar-toggler");
+const navbarLinks = document.getElementById("navbar-links");
+
+// Show/Hide of the navbar links
+navbarToggler.addEventListener("click", () => navbarLinks.classList.toggle("show"));
