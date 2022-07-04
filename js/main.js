0944f1e32d586a3deaddd676648af26fb7523d52
Tolotra Rabefaly <tolotor@yahoo.fr>
2022-07-04 23:42:08 +0300

    Script for opening and closing navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-// Navbar toggler
+// Navbar elements
 const navbarToggler = document.getElementById("navbar-toggler");
 const navbarLinks = document.getElementById("navbar-links");
 
+// Navbar open state
+let navbarIsOpen = false;
+
+function handleNavbarToggle() {
+    navbarIsOpen = !navbarIsOpen;
+    if (navbarIsOpen) {
+        navbarLinks.classList.remove("close");
+        navbarLinks.style.display = "block";
+        navbarLinks.classList.add("show");
+    }
+    else {
+        navbarLinks.classList.remove("show");
+        navbarLinks.classList.add("close");
+        navbarLinks.onanimationend = function () {
+            if (navbarLinks.classList.contains("close")) navbarLinks.style.display = "none";
+        }
+    }
+}
+
 // Show/Hide of the navbar links
-navbarToggler.addEventListener("click", () => navbarLinks.classList.toggle("show"));
+navbarToggler.addEventListener("click", handleNavbarToggle);
